593a97238ad1232a4e33ecad882b3f9b91c1c9c8
YTTM <120769366+YTTM@users.noreply.github.com>
2023-12-29 11:15:49 +0100

    working on paths check

--- check.py
+++ check.py
@@ -1,3 +1,6 @@
+import os
+
+
 def configuration(config):
     flags = [False, False, False, False, False, False, False, False, False, False]
     if 'pe_server_license' in config.keys():
@@ -25,4 +28,35 @@ def configuration(config):
 
 
 def paths(config):
-    raise NotImplementedError
+    flags = [False, False, False, False, False, False, False, False, False]
+
+    def isvalidpath(path):
+        try:
+            if os.path.exists(path):
+                return True
+        except Exception:
+            pass
+        return False
+
+    if isvalidpath(config['pe_server_license']) or config['pe_server_license'] is None:
+        flags[0] = 'exists'
+    if isvalidpath(config['bannerlord']) or config['bannerlord'] is None:
+        flags[1] = 'exists'
+        if isvalidpath(f"{config['bannerlord']}/bin/Win64_Shipping_Server/DedicatedCustomServer.Starter.exe"):
+            flags[1] = True
+    if isvalidpath(config['pe']) or config['pe'] is None:
+        flags[2] = 'exists'
+    if isvalidpath(config['pe_submodule_file']) or config['pe_submodule_file'] is None:
+        flags[3] = 'exists'
+    if isvalidpath(config['pe_bin']) or config['pe_bin'] is None:
+        flags[4] = 'exists'
+    if isvalidpath(config['pe_config']) or config['pe_config'] is None:
+        flags[5] = 'exists'
+    if isvalidpath(config['mission']) or config['mission'] is None:
+        flags[6] = 'exists'
+    if isvalidpath(config['starter']) or config['starter'] is None:
+        flags[7] = 'exists'
+    if isvalidpath(config['destination']) or config['destination'] is None:
+        flags[8] = 'exists'
+
+    return flags
