9c5a320d36af662c0559f2689e5bc87ce6b84dca
YTTM <120769366+YTTM@users.noreply.github.com>
2023-12-29 10:22:52 +0100

    update gui and work on config load

--- gui.py
+++ gui.py
@@ -1,8 +1,11 @@
 import base64
+
+import check
 import icon
 
 import tkinter as tk
 import tkinter.ttk as ttk
+import tkinter.messagebox
 from pygubu.widgets.pathchooserinput import PathChooserInput
 
 
@@ -14,124 +17,120 @@ class BaseWindow:
         toplevel1.title("Persistent Empires Server Installer - [14e] Yttrium")
         frame1 = ttk.Frame(toplevel1)
         frame1.configure(height=200, width=200)
-        label1 = ttk.Label(frame1)
-        label1.configure(padding=5, text='Persistent Empires server license')
-        label1.grid(column=0, row=0, sticky="w")
-        label2 = ttk.Label(frame1)
-        label2.configure(padding=5, text='Bannerlord dedicated server')
-        label2.grid(column=0, row=1, sticky="w")
-        label3 = ttk.Label(frame1)
-        label3.configure(padding=5, text='Persistent Empires mod')
-        label3.grid(column=0, row=2, sticky="w")
-        label4 = ttk.Label(frame1)
-        label4.configure(padding=5, text='SubModule.xml')
-        label4.grid(column=0, row=3, sticky="w")
-        label5 = ttk.Label(frame1)
-        label5.configure(padding=5, text='Server binary release')
-        label5.grid(column=0, row=4, sticky="w")
-        label6 = ttk.Label(frame1)
-        label6.configure(padding=5, text='ModuleData configuration files')
-        label6.grid(column=0, row=5, sticky="w")
-        label7 = ttk.Label(frame1)
-        label7.configure(padding=5, text='Bannerlord mission files (.txt)')
-        label7.grid(column=0, row=6, sticky="w")
-        label8 = ttk.Label(frame1)
-        label8.configure(padding=5, text='Server starters (.bat)')
-        label8.grid(column=0, row=7, sticky="w")
+        self.label_pe_server_license = ttk.Label(frame1)
+        self.label_pe_server_license.configure(
+            padding=5, text='Persistent Empires server license')
+        self.label_pe_server_license.grid(column=0, row=0, sticky="w")
+        self.label_bannerlord = ttk.Label(frame1)
+        self.label_bannerlord.configure(
+            padding=5, text='Bannerlord dedicated server')
+        self.label_bannerlord.grid(column=0, row=1, sticky="w")
+        self.label_pe = ttk.Label(frame1)
+        self.label_pe.configure(padding=5, text='Persistent Empires mod')
+        self.label_pe.grid(column=0, row=2, sticky="w")
+        self.label_pe_submodule_file = ttk.Label(frame1)
+        self.label_pe_submodule_file.configure(padding=5, text='SubModule.xml')
+        self.label_pe_submodule_file.grid(column=0, row=3, sticky="w")
+        self.label_pe_bin = ttk.Label(frame1)
+        self.label_pe_bin.configure(padding=5, text='Server binary release')
+        self.label_pe_bin.grid(column=0, row=4, sticky="w")
+        self.label_pe_config = ttk.Label(frame1)
+        self.label_pe_config.configure(
+            padding=5, text='ModuleData configuration files')
+        self.label_pe_config.grid(column=0, row=5, sticky="w")
+        self.label_mission = ttk.Label(frame1)
+        self.label_mission.configure(
+            padding=5, text='Bannerlord mission files (.txt)')
+        self.label_mission.grid(column=0, row=6, sticky="w")
+        self.label_starter = ttk.Label(frame1)
+        self.label_starter.configure(padding=5, text='Server starters (.bat)')
+        self.label_starter.grid(column=0, row=7, sticky="w")
         label9 = ttk.Label(frame1)
         label9.configure(padding=5)
         label9.grid(column=0, row=8, sticky="w")
-        label10 = ttk.Label(frame1)
-        label10.configure(
-            justify="right",
-            padding=5,
-            text='Destination folder')
-        label10.grid(column=0, row=9, sticky="w")
-        pathchooserinput1 = PathChooserInput(frame1)
+        self.label_destination = ttk.Label(frame1)
+        self.label_destination.configure(
+            justify="right", padding=5, text='Destination folder')
+        self.label_destination.grid(column=0, row=9, sticky="w")
+        self.path_pe_server_license = PathChooserInput(frame1)
         self.pe_server_license = tk.StringVar()
-        pathchooserinput1.configure(
+        self.path_pe_server_license.configure(
             mustexist=True,
             textvariable=self.pe_server_license,
             type="directory")
-        pathchooserinput1.grid(column=1, padx=10, row=0)
-        pathchooserinput1.bind(
-            "<<PathChooserPathChanged>>",
-            self.path_changed,
-            add="")
-        pathchooserinput2 = PathChooserInput(frame1)
+        self.path_pe_server_license.grid(column=1, padx=10, row=0)
+        self.path_pe_server_license.bind(
+            "<<PathChooserPathChanged>>", self.path_changed, add="")
+        self.path_bannerlord = PathChooserInput(frame1)
         self.bannerlord = tk.StringVar()
-        pathchooserinput2.configure(
+        self.path_bannerlord.configure(
             mustexist=True,
             textvariable=self.bannerlord,
             type="directory")
-        pathchooserinput2.grid(column=1, padx=10, row=1)
-        pathchooserinput2.bind(
+        self.path_bannerlord.grid(column=1, padx=10, row=1)
+        self.path_bannerlord.bind(
             "<<PathChooserPathChanged>>",
             self.path_changed,
             add="")
-        pathchooserinput3 = PathChooserInput(frame1)
+        self.path_pe = PathChooserInput(frame1)
         self.pe = tk.StringVar()
-        pathchooserinput3.configure(
+        self.path_pe.configure(
             mustexist=True,
             textvariable=self.pe,
             type="directory")
-        pathchooserinput3.grid(column=1, padx=10, row=2)
-        pathchooserinput3.bind(
+        self.path_pe.grid(column=1, padx=10, row=2)
+        self.path_pe.bind(
             "<<PathChooserPathChanged>>",
             self.path_changed,
             add="")
-        pathchooserinput4 = PathChooserInput(frame1)
+        self.path_pe_submodule_file = PathChooserInput(frame1)
         self.pe_submodule_file = tk.StringVar()
-        pathchooserinput4.configure(
-            mustexist=True,
-            textvariable=self.pe_submodule_file,
-            type="file")
-        pathchooserinput4.grid(column=1, padx=10, row=3)
-        pathchooserinput4.bind(
-            "<<PathChooserPathChanged>>",
-            self.path_changed,
-            add="")
-        pathchooserinput5 = PathChooserInput(frame1)
+        self.path_pe_submodule_file.configure(
+            mustexist=True, textvariable=self.pe_submodule_file, type="file")
+        self.path_pe_submodule_file.grid(column=1, padx=10, row=3)
+        self.path_pe_submodule_file.bind(
+            "<<PathChooserPathChanged>>", self.path_changed, add="")
+        self.path_pe_bin = PathChooserInput(frame1)
         self.pe_bin = tk.StringVar()
-        pathchooserinput5.configure(
+        self.path_pe_bin.configure(
             mustexist=True,
             textvariable=self.pe_bin,
             type="directory")
-        pathchooserinput5.grid(column=1, padx=10, row=4)
-        pathchooserinput5.bind(
+        self.path_pe_bin.grid(column=1, padx=10, row=4)
+        self.path_pe_bin.bind(
             "<<PathChooserPathChanged>>",
             self.path_changed,
             add="")
-        pathchooserinput6 = PathChooserInput(frame1)
+        self.path_pe_config = PathChooserInput(frame1)
         self.pe_config = tk.StringVar()
-        pathchooserinput6.configure(
+        self.path_pe_config.configure(
             mustexist=True,
             textvariable=self.pe_config,
             type="directory")
-        pathchooserinput6.grid(column=1, padx=10, row=5)
-        pathchooserinput6.bind(
+        self.path_pe_config.grid(column=1, padx=10, row=5)
+        self.path_pe_config.bind(
             "<<PathChooserPathChanged>>",
             self.path_changed,
             add="")
-        pathchooserinput7 = PathChooserInput(frame1)
+        self.path_mission = PathChooserInput(frame1)
         self.mission = tk.StringVar()
-        pathchooserinput7.configure(
+        self.path_mission.configure(
             mustexist=True,
             textvariable=self.mission,
             type="directory")
-        pathchooserinput7.grid(column=1, padx=10, row=6)
-        pathchooserinput7.bind(
+        self.path_mission.grid(column=1, padx=10, row=6)
+        self.path_mission.bind(
             "<<PathChooserPathChanged>>",
             self.path_changed,
             add="")
-        pathchooserinput8 = PathChooserInput(frame1)
+        self.path_starter = PathChooserInput(frame1)
         self.starter = tk.StringVar()
-        pathchooserinput8.configure(
+        self.path_starter.configure(
             mustexist=True,
             textvariable=self.starter,
             type="directory")
-        pathchooserinput8.grid(column=1, padx=10, row=7)
-        pathchooserinput8.bind(
+        self.path_starter.grid(column=1, padx=10, row=7)
+        self.path_starter.bind(
             "<<PathChooserPathChanged>>",
             self.path_changed,
             add="")
@@ -145,14 +144,14 @@ class BaseWindow:
             "<<PathChooserPathChanged>>",
             self.path_changed,
             add="")
-        pathchooserinput10 = PathChooserInput(frame1)
+        self.path_destination = PathChooserInput(frame1)
         self.destination = tk.StringVar()
-        pathchooserinput10.configure(
+        self.path_destination.configure(
             mustexist=True,
             textvariable=self.destination,
             type="directory")
-        pathchooserinput10.grid(column=1, padx=10, row=9)
-        pathchooserinput10.bind(
+        self.path_destination.grid(column=1, padx=10, row=9)
+        self.path_destination.bind(
             "<<PathChooserPathChanged>>",
             self.path_changed,
             add="")
@@ -168,14 +167,15 @@ class BaseWindow:
         frame1.pack(side="top")
         frame3 = ttk.Frame(toplevel1)
         frame3.configure(height=200, width=200)
-        button11 = ttk.Button(frame3)
-        button11.configure(text='Check', width=20)
-        button11.grid(column=0, row=0)
-        button11.configure(command=self.check)
-        button12 = ttk.Button(frame3)
-        button12.configure(state="disabled", text='Install', width=20)
-        button12.grid(column=1, row=0)
-        button12.configure(command=self.install)
+        self.button_check = ttk.Button(frame3)
+        self.button_check.configure(text='Check', width=20)
+        self.button_check.grid(column=0, row=0)
+        self.button_check.configure(command=self.check)
+        self.button_install = ttk.Button(frame3)
+        self.button_install.configure(
+            state="disabled", text='Install', width=20)
+        self.button_install.grid(column=1, row=0)
+        self.button_install.configure(command=self.install)
         frame3.pack(side="top")
 
         # Main widget
@@ -212,7 +212,7 @@ class BaseWindow:
 
 
 class MainWindow(BaseWindow):
-    def __init__(self):
+    def __init__(self, config=None):
         super().__init__()
 
         self.mainwindow.geometry("+0+0")
@@ -221,10 +221,46 @@ class MainWindow(BaseWindow):
         self.label_show_1.configure(image=self.icon_pe)
         self.label_show_1.image = self.icon_pe
 
+        self.config = dict()
+        if config is not None:
+            self.load_config(config)
+        else:
+            self.path_changed()
+
+    def load_config(self, config):
+        if not all(check.configuration(config)):
+            tk.messagebox.showerror("Load configuration file", "Invalid configuration")
+        else:
+            emptyIfNone = lambda a: '' if a is None else a
+            self.config = config
+            self.pe_server_license.set(emptyIfNone(config['pe_server_license']))
+            self.bannerlord.set(emptyIfNone(config['bannerlord']))
+            self.pe.set(emptyIfNone(config['pe']))
+            self.pe_submodule_file.set(emptyIfNone(config['pe_submodule_file']))
+            self.pe_bin.set(emptyIfNone(config['pe_bin']))
+            self.pe_config.set(emptyIfNone(config['pe_config']))
+            self.mission.set(emptyIfNone(config['mission']))
+            self.starter.set(emptyIfNone(config['starter']))
+            self.destination.set(emptyIfNone(config['destination']))
+
     def path_changed(self, event=None):
-        pass
+        self.button_install['state'] = 'disabled'
+
+        noneIfEmpty = lambda a : None if len(a) == 0 else a
+        self.config['pe_server_license'] = noneIfEmpty(self.pe_server_license.get())
+        self.config['bannerlord'] = noneIfEmpty(self.bannerlord.get())
+        self.config['pe'] = noneIfEmpty(self.pe.get())
+        self.config['pe_submodule_file'] = noneIfEmpty(self.pe_submodule_file.get())
+        self.config['pe_bin'] = noneIfEmpty(self.pe_bin.get())
+        self.config['pe_config'] = noneIfEmpty(self.pe_config.get())
+        self.config['mission'] = noneIfEmpty(self.mission.get())
+        self.config['starter'] = noneIfEmpty(self.starter.get())
+        self.config['destination'] = noneIfEmpty(self.destination.get())
 
     def check(self):
+        if not all(check.configuration(self.config)):
+            tk.messagebox.showerror("Check", "Invalid configuration")
+            return
         pass
 
     def install(self):
