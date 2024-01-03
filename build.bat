3cfa86ba5535a2c4e8fac0a3759451d08b5d3ca3
YTTM <120769366+YTTM@users.noreply.github.com>
2024-01-03 14:10:15 +0100

    update build script to rename release file

--- build.bat
+++ build.bat
@@ -1,1 +1,2 @@
 pyinstaller --specpath ./release/spec --distpath ./release --workpath ./release --icon=../../icon/bannerlord_icon.ico --onefile main.py
+COPY .\release\main.exe .\release\PersistentEmpiresServerInstaller.exe
